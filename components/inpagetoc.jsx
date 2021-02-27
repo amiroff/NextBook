db50b73aebb9bdcae8b76bd18c278462b1112fd3
Metin Emiroğlu <amiroff@gmail.com>
2021-02-27 23:35:27 +0300

    In-page TOC indentation

--- components/inpagetoc.jsx
+++ components/inpagetoc.jsx
@@ -1,18 +1,35 @@
 import Scrollspy from 'react-scrollspy-ez'
 import Text from './text'
 
+function InPageTocElement(props) {
+  // remove 1 from first heading to prevent exe left padding and add standard left padding
+  const style = { paddingLeft: `${props.levels[props.children] - 1 + 0.5}em` }
+  return (
+    <li style={style} className={props.className}>
+      {props.children}
+    </li>
+  )
+}
+
 function InPageToc({ tocRaw }) {
   let tocIds = []
+  let levels = {}
   tocRaw.forEach((row) => {
+    // populate dictionary of heading slugs
     tocIds.push(row.slug)
+    // populate dictionary of headings and their levels
+    levels[row.content] = row.lvl
   })
 
   if (tocIds.length) {
     return (
       <div className='content position-fixed page-toc top-10'>
-        <h2 className='content-title'><Text tid='Page Contents' /></h2>
+        <h2 className='content-title'>
+          <Text tid='Page Contents' />
+        </h2>
         <Scrollspy
           ids={tocIds}
+          itemElement={<InPageTocElement levels={levels} />}
           itemContainerClassName='page-toc-container'
           activeItemClassName='page-toc-active'
           includeParentClasses={true}
