2fa5158fb93a1232836b4a2d281b372ec3f92072
Metin Emiroğlu <amiroff@gmail.com>
2021-03-04 19:55:39 +0300

    Replace global font, more modern search input, tweak marking color.

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -9,7 +9,6 @@ function SideBar({ toc }) {
   const router = useRouter()
   const [chapters, setChapters] = useLocalStorage('visitedChapters', [])
   const [keyword, setKeyword] = useState('')
-  const [clearVisible, setClearVisible] = useState(false)
   const [filteredToc, setFilteredToc] = useState(toc)
   const searchInput = useRef(null)
 
@@ -17,14 +16,7 @@ function SideBar({ toc }) {
     searchInput.current.focus()
   }
 
-  const clear = () => {
-    setKeyword('')
-    setClearVisible(false)
-    focus()
-  }
-
   useShortcuts(['control', 'shift', 'F'], () => focus())
-  useShortcuts(['Escape'], () => clear())
 
   useEffect(() => {
     // update history array with every page visit
@@ -43,10 +35,8 @@ function SideBar({ toc }) {
         )
         return newItem
       })
-      setClearVisible(true)
       setFilteredToc(filteredToc)
     } else {
-      setClearVisible(false)
       setFilteredToc(toc)
     }
   }, [keyword])
@@ -55,40 +45,19 @@ function SideBar({ toc }) {
     <>
       <div className='sidebar'>
         <div className='sidebar-content'>
-          <div className='input-group'>
-            <label htmlFor='search' className='sr-only'>
-              <Text tid='Filter content' />
-            </label>
-            <input
-              id='search'
-              className='form-control search rounded'
-              type='text'
-              placeholder={_('Filter content')}
-              ref={searchInput}
-              value={keyword}
-              onChange={(e) => setKeyword(e.target.value)}
-            />
-            {clearVisible && (
-              <div className='input-group-append'>
-                <button className='btn p-0' type='button' onClick={clear} title={_('Reset search')}>
-                  <svg
-                    className='w-6 h-6 svg-button'
-                    fill='none'
-                    stroke='currentColor'
-                    viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth={2}
-                      d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-                    />
-                  </svg>
-                </button>
-              </div>
-            )}
-          </div>
+          <label htmlFor='search' className='sr-only' autoC>
+            <Text tid='Filter content' />
+          </label>
+          <input
+            id='search'
+            className='form-control search rounded font-size-14'
+            type='search'
+            placeholder={_('Filter content')}
+            ref={searchInput}
+            value={keyword}
+            autoComplete='off'
+            onChange={(e) => setKeyword(e.target.value)}
+          />
           <div className='mt-10 font-size-12 hidden-sm-and-down text-muted'>
             <kbd className='text-muted'>ctrl</kbd> + <kbd className='text-muted'>shift</kbd> +{' '}
             <kbd className='text-muted'>F</kbd>
