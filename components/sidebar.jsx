bed561f8f7e6816890e8c7042e758230da0223c5
Metin Emiroğlu <amiroff@gmail.com>
2021-03-16 19:50:00 +0300

    Remove sidebar filter for future (and better) re-consideration

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,22 +1,13 @@
 import { useRouter } from 'next/router'
-import { useEffect, useRef, useState } from 'react'
-import { useShortcuts } from 'react-shortcuts-hook'
+import { useEffect } from 'react'
 import { useLocalStorage } from 'react-use'
 import SideBarSection from './sidebarsection'
-import Text, { _ } from './text'
+import Link from 'next/link'
+import config from '../config.json'
 
-function SideBar({ toc, part }) {
+function SideBar({ toc, part, docTitle }) {
   const router = useRouter()
   const [chapters, setChapters] = useLocalStorage('visitedChapters', [])
-  const [keyword, setKeyword] = useState('')
-  const [filteredToc, setFilteredToc] = useState(toc)
-  const searchInput = useRef(null)
-
-  const focus = () => {
-    searchInput.current.focus()
-  }
-
-  useShortcuts(['control', 'shift', 'F'], () => focus())
 
   useEffect(() => {
     // update history array with every page visit
@@ -25,46 +16,23 @@ function SideBar({ toc, part }) {
     }
   }, [chapters, router.pathname])
 
-  useEffect(() => {
-    if (keyword !== '') {
-      // keep only parts with chapters matching search criteria
-      const filteredToc = toc.map((item) => {
-        const newItem = { part: item.part }
-        newItem.chapters = item.chapters.filter(({ title }) =>
-          title.toLowerCase().includes(keyword.toLowerCase())
-        )
-        return newItem
-      })
-      setFilteredToc(filteredToc)
-    } else {
-      setFilteredToc(toc)
-    }
-  }, [keyword])
-
   return (
     <>
       <div className='sidebar'>
         <div className='sidebar-content'>
-          <label htmlFor='search' className='sr-only'>
-            <Text tid='Filter content' />
-          </label>
-          <input
-            id='search'
-            className='form-control search rounded font-size-14'
-            type='search'
-            placeholder={_('Filter content')}
-            ref={searchInput}
-            value={keyword}
-            autoComplete='off'
-            onChange={(e) => setKeyword(e.target.value)}
-          />
-          <div className='mt-10 font-size-12 hidden-sm-and-down text-muted'>
-            <kbd className='text-muted'>ctrl</kbd> + <kbd className='text-muted'>shift</kbd> +{' '}
-            <kbd className='text-muted'>F</kbd>
+          <div className='mx-auto my-20 font-size-14 font-weight-bold'>
+            <Link href='/'>
+              <a className='text-decoration-none d-flex flex-column align-items-center'>
+                {config.useTitleLogo && (
+                  <img src='/icon.svg' alt='NextBook Logo' className='mb-10 w-100' />
+                )}
+                {docTitle}
+              </a>
+            </Link>
           </div>
         </div>
         <div className='sidebar-menu'>
-          {filteredToc.map((toc, id) => (
+          {toc.map((toc, id) => (
             <SideBarSection
               toc={toc}
               part={part}
@@ -73,6 +41,17 @@ function SideBar({ toc, part }) {
               key={id}
             />
           ))}
+        </div>
+        <div className='mx-auto w-100'>
+          <iframe
+            src='https://ghbtns.com/github-btn.html?user=amiroff&repo=nextbook&type=star&count=true'
+            frameBorder='0'
+            scrolling='0'
+            width='90'
+            height='20'
+            title='NextBook on Github'
+            className='mt-5 d-none d-md-block'
+          ></iframe>
         </div>
       </div>
     </>
