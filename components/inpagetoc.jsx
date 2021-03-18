e678383bcfaf2632194af21e68796a99b9febfe3
Metin Emiroğlu <amiroff@gmail.com>
2021-03-18 21:54:21 +0300

    Sizing tweaks

--- components/inpagetoc.jsx
+++ components/inpagetoc.jsx
@@ -3,7 +3,7 @@ import Text from './text'
 
 function InPageTocElement(props) {
   // remove 1 from first heading to prevent exe left padding and add standard left padding
-  const style = { paddingLeft: `${props.levels[props.children] - 1 + 0.5}em` }
+  const style = { paddingLeft: `${props.levels[props.children] - 1 + 0.2}em` }
   return (
     <li style={style} className={props.className} {...props}>
       {props.children}
@@ -23,8 +23,8 @@ function InPageToc({ tocRaw }) {
 
   if (tocIds.length) {
     return (
-      <div className='content position-fixed page-toc top-10 mr-5'>
-        <span className='content-title m-20 ml-0 font-size-18 font-weight-bold'>
+      <div className='content position-fixed page-toc top9-10 mr-15'>
+        <span className='content-title'>
           <Text tid='Page Contents' />
         </span>
         <Scrollspy
