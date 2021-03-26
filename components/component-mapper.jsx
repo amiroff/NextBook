52d5ee19eaedf942d010e40325c08cdadafaf8d8
Metin Emiroğlu <amiroff@gmail.com>
2021-03-26 15:24:52 +0300

    Extract component mapper

---
+++ components/component-mapper.jsx
@@ -0,0 +1,37 @@
+import {
+  Blockquote,
+  Code,
+  CustomLink,
+  Details,
+  Image,
+  Pre,
+  Summary,
+  Tab,
+  Table,
+  Tabs,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6
+} from './mdxcomponents'
+
+export const componentMap = {
+  table: Table,
+  a: CustomLink,
+  img: Image,
+  blockquote: Blockquote,
+  pre: Pre,
+  details: Details,
+  summary: Summary,
+  code: Code,
+  tabs: Tabs,
+  tab: Tab,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,48 +1,12 @@
 import { MDXProvider } from '@mdx-js/react'
 import splitbee from '@splitbee/web'
 import { SideBarContextProvider } from 'components/context'
-import {
-  Blockquote,
-  Code,
-  CustomLink,
-  Details,
-  Image,
-  Pre,
-  Summary,
-  Tab,
-  Table,
-  Tabs,
-  H1,
-  H2,
-  H3,
-  H4,
-  H5,
-  H6
-} from 'components/mdxcomponents'
 import withDarkMode from 'next-dark-mode'
+import { componentMap } from 'components/component-mapper'
 import config from '../config.json'
 import '../styles/halfmoon-variables.min.css'
 import '../styles/global.css'
 import '../styles/custom.css'
-
-const components = {
-  table: Table,
-  a: CustomLink,
-  img: Image,
-  blockquote: Blockquote,
-  pre: Pre,
-  details: Details,
-  summary: Summary,
-  code: Code,
-  tabs: Tabs,
-  tab: Tab,
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  h5: H5,
-  h6: H6
-}
 
 if (
   process.env.NEXT_PUBLIC_ENVIRONMENT !== 'development' &&
@@ -56,7 +20,7 @@ if (
 function MyApp({ Component, pageProps }) {
   return (
     <SideBarContextProvider>
-      <MDXProvider components={components}>
+      <MDXProvider components={componentMap}>
         <Component {...pageProps} />
       </MDXProvider>
     </SideBarContextProvider>
