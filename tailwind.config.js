a811224174504f4fec2c11d5a851eca980d68535
Metin Emiroğlu <amiroff@gmail.com>
2021-05-02 02:03:02 +0300

    Disable tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  mode: 'jit', // Has some bugs not picking up styles. Comment out this line if you have these.
+  // mode: 'jit', // Has some bugs not picking up styles. Comment out this line if you have these.
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
