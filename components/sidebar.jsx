f3b150afa56914e4a401fefedf9af563bb364443
Metin Emiroğlu <amiroff@gmail.com>
2021-05-02 02:15:26 +0300

    Close sidebar on mobile on click outside

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,14 +1,15 @@
 import SideBarContext from 'components/store/sidebar-context'
 import config from 'config/config.json'
 import Link from 'next/link'
-import { useContext, useEffect } from 'react'
-import { useMedia } from 'react-use'
+import { useContext, useEffect, useRef } from 'react'
+import { useClickAway, useMedia } from 'react-use'
 import SideBarSection from './sidebar-section'
 
 function SideBar() {
   const { toc, projectTitle } = config
   const sideBarCtx = useContext(SideBarContext)
   const isWide = useMedia('(min-width: 770px)')
+  const ref = useRef(null)
 
   useEffect(() => {
     if (isWide) {
@@ -16,13 +17,19 @@ function SideBar() {
     }
   }, [isWide])
 
+  useClickAway(ref, () => {
+    if (!isWide) {
+      sideBarCtx.hideSideBar()
+    }
+  })
+
   const sideBarStyle = sideBarCtx.sideBar
-    ? 'sidebar w-2/3 z-50 h-screen bg-gray-300 dark:bg-gray-900 border-r border-gray-400 dark:border-gray-800 fixed pl-4 pr-6 text-lg top-10 md:hidden'
+    ? 'sidebar w-2/3 z-50 h-screen bg-gray-100 dark:bg-gray-900 border-r border-gray-300 dark:border-gray-800 fixed pl-4 text-lg top-10 md:hidden'
     : 'sidebar z-50 flex-none md:w-56 xl:w-64 h-screen fixed top-10 md:top-14 hidden md:block'
 
   return (
-    <aside className={sideBarStyle}>
-      <div className='w-full pb-40 md:pb-16 h-full overflow-y-auto'>
+    <aside className={sideBarStyle} ref={ref}>
+      <div className='w-full pb-40 md:pb-16 h-full overflow-y-auto pr-6'>
         <div className='flex flex-col md:mt-10'>
           <Link href='/'>
             <a href='/' aria-label={projectTitle}>
