7ec3378f23c21e91cd52b3e7b9bfc45aa4924a90
Metin Emiroğlu <amiroff@gmail.com>
2021-06-15 19:45:42 +0300

    Next11, Webpack 5, TWJIT enabled.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  // mode: 'jit', // Has some bugs not picking up styles. Comment out this line if you have these.
+  mode: 'jit',
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
