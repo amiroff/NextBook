945fcc0399a7d6fb6c3bb0b7f20acae53d077aa4
Metin Emiroglu <amiroff@gmail.com>
2022-01-08 20:23:35 +0300

    Feature: add index page fallback

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 export default function HomePage() {
-  const docsIndex = '/intro/index'
+  const docsIndex = '/intro'
   const router = useRouter()
 
   useEffect(() => {
