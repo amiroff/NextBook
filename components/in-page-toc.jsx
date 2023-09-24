794d05ea5a62a8069a772615384c50953e6cc9a6
Lacy Morrow <me@lacymorrow.com>
2023-09-23 22:43:15 -0400

    fix scrollspy; fix copy to clipboard

--- components/in-page-toc.jsx
+++ components/in-page-toc.jsx
@@ -5,74 +5,72 @@ import Text from './text'
 import { Link } from './svg-icons';
 
 function InPageTocElement(props) {
-  // Indent headings based on their level
-  const level = props.levels[props.slug] >= 1 ? props.levels[props.slug] : 1
-  const style = { paddingLeft: `${level/2}em` }
-  return (
-    <li style={style} className={props.className} onClick={props.onClick}>
-      {props.children}
-    </li>
-  )
+	// Indent headings based on their level
+	const level = props.levels[props.slug] >= 1 ? props.levels[props.slug] : 1
+	const style = { paddingLeft: `${level / 2}em` }
+	return (
+		<li style={style} className={props.className} onClick={props.onClick}>
+			{props.children}
+		</li>
+	)
 }
 
 function InPageToc({ tocRaw }) {
-  const [tocIds, setTocIds] = useState([])
-  const [levels, setLevels] = useState({})
 
-  useEffect(() => {
-    let tmpIds = []
-    let tmpLevels = {}
-    tocRaw.forEach((row) => {
-      // populate dictionary of heading slugs
-      tmpIds.push(row.slug)
-      // populate dictionary of headings and their levels
-      tmpLevels[row.slug] = row.lvl
-    })
-    setTocIds(tmpIds)
-    setLevels(tmpLevels)
-  }, [tocRaw])
+	let tocIds = []
+	let levels = {}
+	tocRaw.forEach((row) => {
+		// populate dictionary of heading slugs
+		tocIds.push(row.slug)
+		// populate dictionary of headings and their levels
+		levels[row.slug] = row.lvl
+	})
 
-  // Update current "copy link" button when the user scrolls
-  const [url, setUrl] = useState('')
+	// Update current "copy link" button when the user scrolls
+	const [url, setUrl] = useState('')
 
-  const onUpdateHash = (hash) => {
-    setUrl(window.location.origin + window.location.pathname + hash)
-  }
+	const onUpdateHash = (hash) => {
+		setUrl(window.location.origin + window.location.pathname + hash)
+	}
 
-  // on mount, set the initial hash
-  useEffect(()=>{
-    setUrl(router.asPath)
-  }, []);
+	// on mount, set the initial hash
+	useEffect(() => {
+		setUrl(router.asPath)
+	}, []);
 
-  if (tocIds.length) {
-    return (
-      <div className='px-4'>
-        <a
-          className='m-4 text-sm text-gray-400 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-300 block'
-          href={url}
-        >
-          <Link />
-          <Text tid='Permalink' className="px-4" />
-        </a>
+	if (tocIds.length) {
+		return (
+			<div className='px-4'>
+				<a
+					className='m-4 text-sm text-gray-400 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-300 block'
+					href={url}
+					onClick={(e) => {
+						e.preventDefault();
+						navigator.clipboard.writeText(url);
+					}}
+				>
+					<Link />
+					<Text tid='Permalink' className="px-4" />
+				</a>
 
-        <div className='py-2 text-gray-400 dark:text-gray-300 uppercase text-sm font-bold'>
-          <Text tid='Page Contents' className="tracking-widest" />
-        </div>
-        <Scrollspy
-          ids={tocIds}
-          itemElement={<InPageTocElement levels={levels} />}
-          itemContainerClassName='tracking-wide mt-4 text-gray-600 dark:text-gray-400 text-xs 2xl:text-sm border-0 border-l border-gray-200 dark:border-gray-600 leading-6 cursor-pointer'
-          itemClassName="text-gray-500 truncate py-1 hover:text-gray-700 dark:hover:text-gray-300"
-          activeItemClassName='active text-gray-700 dark:text-gray-200 border-l-2 border-gray-900 dark:border-gray-200'
-          includeParentClasses={false}
-          onUpdateHash={onUpdateHash}
-        />
-        
-      </div>
-    )
-  } else {
-    return null
-  }
+				<div className='py-2 text-gray-400 dark:text-gray-300 uppercase text-sm font-bold'>
+					<Text tid='Page Contents' className="tracking-widest" />
+				</div>
+				<Scrollspy
+					ids={tocIds}
+					itemElement={<InPageTocElement levels={levels} />}
+					itemContainerClassName='tracking-wide mt-4 text-gray-600 dark:text-gray-400 text-xs 2xl:text-sm border-0 border-l border-gray-200 dark:border-gray-600 leading-6 cursor-pointer'
+					itemClassName="text-gray-500 truncate py-1 hover:text-gray-700 dark:hover:text-gray-300"
+					activeItemClassName='active text-gray-700 dark:text-gray-200 border-l-2 border-gray-900 dark:border-gray-200'
+					includeParentClasses={false}
+					onUpdateHash={onUpdateHash}
+				/>
+
+			</div>
+		)
+	} else {
+		return null
+	}
 }
 
 export default InPageToc
--- components/scrollspy.jsx
+++ components/scrollspy.jsx
@@ -2,7 +2,7 @@
  * @class Scrollspy
  * Taken verbatim from https://github.com/telega/react-scrollspy-ez
  * and stripped of TS interfaces.
- * 
+ *
  * Lacy - November 2022: Added nextJS router support
  */
 
@@ -15,151 +15,151 @@ import throttle from 'utils/throttle'
 const DEBOUNCE_HASH_DELAY = 300
 const THROTTLE_DELAY = 20
 export default class Scrollspy extends React.Component {
-  constructor(props) {
-    super(props)
-    this.elements = []
-    this.state = {
-      items: [],
-      current: ''
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.elements = []
+		this.state = {
+			items: [],
+			current: ''
+		}
+	}
 
-  updateHash = (hash) => {
-    // enforce hash # prefix
-    if ((''+hash).charAt(0) !== '#') hash = '#' + hash;
-    
-    // router.replace causes a scroll
-    history.replaceState({}, '', hash);
+	updateHash = (hash) => {
+		// enforce hash # prefix
+		if (('' + hash).charAt(0) !== '#') hash = '#' + hash;
 
-    this.props.onUpdateHash(hash)
-  }
+		// router.replace causes a scroll
+		history.replaceState({}, '', hash);
 
-  dUpdateHash = debounce(this.updateHash, DEBOUNCE_HASH_DELAY)
+		this.props.onUpdateHash(hash)
+	}
 
-  spy() {
-    // I don't understand why this was implemented this way, but I optimized it
-    if(this.elements) {
-      const items = this.elements
-        .map((element) => {
-          if (element) {
-            return {
-              id: element.id,
-              inView: this.isInView(element),
-              element
-            }
-          } else {
-            return
-          }
-        })
+	dUpdateHash = debounce(this.updateHash, DEBOUNCE_HASH_DELAY)
 
-      const itemInView = items.find((item) => !!item && item.inView)
+	spy() {
+		// I don't understand why this was implemented this way, but I optimized it
+		if (this.elements) {
+			const items = this.elements
+				.map((element) => {
+					if (element) {
+						return {
+							id: element.id,
+							inView: this.isInView(element),
+							element
+						}
+					} else {
+						return
+					}
+				})
 
-      if (!itemInView) {
-        return
-      }
+			let itemInView = items.find((item) => !!item && item.inView)
 
-      if(this.state.current !== itemInView.id) {
-        // item updated
+			if (!itemInView) {
+				itemInView = items[0]
+			}
 
-        const update = items.map((item) => {
-          return { ...item, inView: item === itemInView }
-        })
+			if (this.state.current !== itemInView.id) {
+				// item updated
 
-        this.setState({ items: update, current: itemInView.id  })
+				const update = items.map((item) => {
+					return { ...item, inView: item === itemInView }
+				})
 
-        // update page hash
-        this.dUpdateHash(itemInView.id)
+				this.setState({ items: update, current: itemInView.id })
 
-      }
-    }
-  }
+				// update page hash
+				this.dUpdateHash(itemInView.id)
 
-  tSpy = throttle(this.spy, THROTTLE_DELAY)
+			}
+		}
+	}
 
-  componentDidMount() {
-    this.elements = this.props.ids.map((id) => document.getElementById(id))
-    window.addEventListener('scroll', () => this.tSpy(), false);
-    window.addEventListener('resize', () => this.tSpy(), false);
-    // fire on router change
-    // router.events.on('routeChangeComplete', () => this.tSpy())
+	tSpy = throttle(this.spy, THROTTLE_DELAY)
 
-    // run on mount
-    this.spy()
-    }
+	componentDidMount() {
+		this.elements = this.props.ids.map((id) => document.getElementById(id))
+		window.addEventListener('scroll', () => this.tSpy(), false);
+		window.addEventListener('resize', () => this.tSpy(), false);
+		// fire on router change
+		// router.events.on('routeChangeComplete', () => this.tSpy())
 
-  componentDidUpdate() {
-    this.elements = this.props.ids.map((id) => document.getElementById(id))
-    this.spy()
-  }
+		// run on mount
+		this.spy()
+	}
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', () => this.tSpy(), false);
-    window.removeEventListener('resize', () => this.tSpy(), false);
-    // router.events.off('routeChangeComplete', () => this.tSpy())
+	componentDidUpdate() {
+		this.elements = this.props.ids.map((id) => document.getElementById(id))
+		this.spy()
+	}
 
-  }
+	componentWillUnmount() {
+		window.removeEventListener('scroll', () => this.tSpy(), false);
+		window.removeEventListener('resize', () => this.tSpy(), false);
+		// router.events.off('routeChangeComplete', () => this.tSpy())
 
-  isInView = (element) => {
-    if (!element) {
-      return false
-    }
-    const { offset } = this.props
-    const rect = element.getBoundingClientRect() 
-    return rect.top >= 0 - offset && rect.bottom <= window.innerHeight + offset
-  }
+	}
 
-  scrollTo(element) {
-    element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest'
-    })
-  }
+	isInView = (element) => {
+		if (!element) {
+			return false
+		}
+		const { offset } = this.props
+		const rect = element.getBoundingClientRect()
+		return rect.top >= 0 - offset && rect.bottom <= window.innerHeight + offset
+	}
 
-  renderItems() {
-    const { itemElement, activeItemClassName, itemClassName } = this.props
-    return this.state.items.map((item, k) => {
-      return itemElement
-        ? React.cloneElement(itemElement, {
-            key: k,
-            className: classnames(
-              this.props.includeParentClasses && item.element.className
-                ? item.element.className
-                : null,
-              itemClassName,
-              item.inView ? activeItemClassName : null
-            ),
-            onClick: () => {
-              // use next router to update url hash
-              this.dUpdateHash(item.id)
+	scrollTo(element) {
+		element.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+			inline: 'nearest'
+		})
+	}
 
-              // scroll to the element
-              this.scrollTo(item.element)
-            },
-            slug: item.id,
-            children: item?.element?.innerText,
-            // Truncate the text to n characters
-            // children: item.element.innerText.replace(/(.{20})..+/, "$1…")
-          })
-        : null
-    })
-  }
+	renderItems() {
+		const { itemElement, activeItemClassName, itemClassName } = this.props
+		return this.state.items.map((item, k) => {
+			return itemElement
+				? React.cloneElement(itemElement, {
+					key: k,
+					className: classnames(
+						this.props.includeParentClasses && item.element.className
+							? item.element.className
+							: null,
+						itemClassName,
+						item.inView ? activeItemClassName : null
+					),
+					onClick: () => {
+						// use next router to update url hash
+						this.dUpdateHash(item.id)
 
-  render() {
-    const { itemContainerClassName, containerElement } = this.props
-    return containerElement && this.state.items
-      ? React.cloneElement(containerElement, {
-          className: classnames(itemContainerClassName),
-          children: this.renderItems()
-        })
-      : null
-  }
+						// scroll to the element
+						this.scrollTo(item.element)
+					},
+					slug: item.id,
+					children: item?.element?.innerText,
+					// Truncate the text to n characters
+					// children: item.element.innerText.replace(/(.{20})..+/, "$1…")
+				})
+				: null
+		})
+	}
+
+	render() {
+		const { itemContainerClassName, containerElement } = this.props
+		return containerElement && this.state.items
+			? React.cloneElement(containerElement, {
+				className: classnames(itemContainerClassName),
+				children: this.renderItems()
+			})
+			: null
+	}
 }
 
 Scrollspy.defaultProps = {
-  offset: 2,
-  ids: [],
-  containerElement: <ul />,
-  itemElement: <li />,
-  includeParentClasses: false
+	offset: 2,
+	ids: [],
+	containerElement: <ul />,
+	itemElement: <li />,
+	includeParentClasses: false
 }
