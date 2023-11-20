e5306626833a100bd73d1144ed0efd1422c562e6
Lacy Morrow <me@lacymorrow.com>
2023-11-20 00:33:26 -0500

    swap

--- components/component-mapper.jsx
+++ components/component-mapper.jsx
@@ -1,29 +1,29 @@
-import { AirtableBase, AirtableForm } from "mdx-embed/dist/components/airtable";
-import { Buzzsprout } from "mdx-embed/dist/components/buzzsprout";
-import { Cinnamon } from "mdx-embed/dist/components/cinnamon";
-import { CodePen } from "mdx-embed/dist/components/codepen";
-import { CodeSandbox } from "mdx-embed/dist/components/codesandbox";
-import { EggheadLesson } from "mdx-embed/dist/components/egghead";
-import { Figma } from "mdx-embed/dist/components/figma";
-import { Flickr } from "mdx-embed/dist/components/flickr";
-import { Gist } from "mdx-embed/dist/components/gist";
-import { Instagram } from "mdx-embed/dist/components/instagram";
-import { Lbry } from "mdx-embed/dist/components/lbry";
-import { LinkedInBadge } from "mdx-embed/dist/components/linkedin";
-import { Pin, PinterestBoard, PinterestFollowButton } from "mdx-embed/dist/components/pinterest";
-import { Replit } from "mdx-embed/dist/components/replit";
-import { SimplecastEpisode } from "mdx-embed/dist/components/simplecast";
-import { Snack } from "mdx-embed/dist/components/snack";
-import { SoundCloud } from "mdx-embed/dist/components/soundcloud";
-import { Spotify } from "mdx-embed/dist/components/spotify";
-import { TikTok } from "mdx-embed/dist/components/tiktok";
-import { Twitch } from "mdx-embed/dist/components/twitch";
-import { Tweet, TwitterFollowButton, TwitterHashtagButton, TwitterList, TwitterMentionButton, TwitterTimeline } from "mdx-embed/dist/components/twitter";
-import { Vimeo } from "mdx-embed/dist/components/vimeo";
-import { Whimsical } from "mdx-embed/dist/components/whimsical";
-import { Wikipedia } from "mdx-embed/dist/components/wikipedia";
-import { Wistia } from "mdx-embed/dist/components/wistia";
-import { YouTube } from "mdx-embed/dist/components/youtube";
+import { AirtableBase, AirtableForm } from "@lacymorrow/mdx-embed/dist/components/airtable";
+import { Buzzsprout } from "@lacymorrow/mdx-embed/dist/components/buzzsprout";
+import { Cinnamon } from "@lacymorrow/mdx-embed/dist/components/cinnamon";
+import { CodePen } from "@lacymorrow/mdx-embed/dist/components/codepen";
+import { CodeSandbox } from "@lacymorrow/mdx-embed/dist/components/codesandbox";
+import { EggheadLesson } from "@lacymorrow/mdx-embed/dist/components/egghead";
+import { Figma } from "@lacymorrow/mdx-embed/dist/components/figma";
+import { Flickr } from "@lacymorrow/mdx-embed/dist/components/flickr";
+import { Gist } from "@lacymorrow/mdx-embed/dist/components/gist";
+import { Instagram } from "@lacymorrow/mdx-embed/dist/components/instagram";
+import { Lbry } from "@lacymorrow/mdx-embed/dist/components/lbry";
+import { LinkedInBadge } from "@lacymorrow/mdx-embed/dist/components/linkedin";
+import { Pin, PinterestBoard, PinterestFollowButton } from "@lacymorrow/mdx-embed/dist/components/pinterest";
+import { Replit } from "@lacymorrow/mdx-embed/dist/components/replit";
+import { SimplecastEpisode } from "@lacymorrow/mdx-embed/dist/components/simplecast";
+import { Snack } from "@lacymorrow/mdx-embed/dist/components/snack";
+import { SoundCloud } from "@lacymorrow/mdx-embed/dist/components/soundcloud";
+import { Spotify } from "@lacymorrow/mdx-embed/dist/components/spotify";
+import { TikTok } from "@lacymorrow/mdx-embed/dist/components/tiktok";
+import { Twitch } from "@lacymorrow/mdx-embed/dist/components/twitch";
+import { Tweet, TwitterFollowButton, TwitterHashtagButton, TwitterList, TwitterMentionButton, TwitterTimeline } from "@lacymorrow/mdx-embed/dist/components/twitter";
+import { Vimeo } from "@lacymorrow/mdx-embed/dist/components/vimeo";
+import { Whimsical } from "@lacymorrow/mdx-embed/dist/components/whimsical";
+import { Wikipedia } from "@lacymorrow/mdx-embed/dist/components/wikipedia";
+import { Wistia } from "@lacymorrow/mdx-embed/dist/components/wistia";
+import { YouTube } from "@lacymorrow/mdx-embed/dist/components/youtube";
 
 import Counter from './example-counter';
 import {
@@ -81,7 +81,7 @@ export const componentMap = {
 	hr: Hr,
 	p: P,
 
-	// mdx-embed
+	// @lacymorrow/mdx-embed
 	AirtableBase,
 	AirtableForm,
 	Buzzsprout,
